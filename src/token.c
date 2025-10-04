6a7b92e1e07f8914df591d5719770c450a97f038
Ragib Asif <ragibasif@tuta.io>
2025-10-04 07:45:29 -0400

    refactor(token): update token implementation

--- src/token.c
+++ src/token.c
@@ -9,15 +9,70 @@
  * Version 1.0.0
  */
 
-#include "nem.h"
+#include "token.h"
 
-#include <stdio.h>
+// TODO: add source file name
+// TODO: add line number
+struct NemToken {
+    enum NemTokenType type;
+    char             *lexeme;
+};
 
-void nem_token_create( struct NemToken *token, char *literal );
+struct NemToken *nem_token_create( enum NemTokenType type,
+                                   const char       *lexeme ) {
 
-void nem_token_destroy( struct NemToken *token );
+    if ( type <= NTT_UNKNOWN || type >= NTT_COUNT || lexeme == NULL ) {
+        errno = EINVAL;
+        return NULL;
+    }
 
-void nem_token_print( struct NemToken *token ) {
-    printf( "%s\n", token->name );
-    printf( "%s\n", token->literal );
+    struct NemToken *nem_token = malloc( sizeof *nem_token );
+
+    if ( nem_token == NULL ) {
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    nem_token->type = type;
+
+    const size_t lexeme_len =
+        sizeof *nem_token->lexeme * ( strlen( lexeme ) + 1 );
+    nem_token->lexeme = malloc( lexeme_len );
+
+    if ( nem_token->lexeme == NULL ) {
+        errno = ENOMEM;
+        return NULL;
+    }
+    memcpy( nem_token->lexeme, lexeme, lexeme_len );
+
+#ifdef NEM_DEBUG
+    fprintf( stderr, "[%s:%d (%s)] type: %d -- lexeme: %s\n", __FILE__,
+             __LINE__, __func__, nem_token->type, nem_token->lexeme );
+#endif // NEM_DEBUG
+
+    return nem_token;
+}
+
+void nem_token_destroy( struct NemToken **token ) {
+    if ( token == NULL ) { return; }
+    if ( *token == NULL ) { return; }
+
+    free( ( *token )->lexeme );
+    ( *token )->lexeme = NULL;
+
+    ( *token )->type = 0;
+
+    free( *token );
+    *token = NULL;
+}
+
+const enum NemTokenType nem_token_type( struct NemToken *token ) {
+    if ( token == NULL ) { return -1; }
+    return token->type;
+}
+
+const char *const nem_token_lexeme( struct NemToken *token ) {
+    if ( token == NULL ) { return NULL; }
+    if ( token->lexeme == NULL ) { return NULL; }
+    return token->lexeme;
 }
---
+++ src/token.h
@@ -0,0 +1,169 @@
+/*
+ * File: token.h
+ * Author: Ragib Asif
+ * Email: ragibasif@tuta.io
+ * GitHub: https://github.com/ragibasif
+ * LinkedIn: https://www.linkedin.com/in/ragibasif/
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2025 Ragib Asif
+ * Version 1.0.0
+ */
+
+#ifndef TOKEN_H
+#define TOKEN_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "common.h"
+
+enum NemTokenType {
+    // Special tokens
+    NTT_UNKNOWN,
+    NTT_EOF,
+    NTT_COMMENT,
+
+    // Literals
+    NTT_IDENTIFIER, // main
+    NTT_NUMBER,     // 12345
+    NTT_DIGIT,      // [0-9]
+    NTT_INTEGER,    // 12345
+    NTT_DOUBLE,
+    NTT_FLOAT,     // 123.45
+    NTT_CHARACTER, // 'a'
+    NTT_STRING,    // "abc"
+    NTT_CONSTANT,
+
+    // Operators
+    NTT_PLUS,     // +
+    NTT_MINUS,    // -
+    NTT_MULTIPLY, // *
+    NTT_DIVIDE,   // /
+    NTT_MODULO,   // %
+    NTT_INVERT,   // ~
+
+    NTT_BITWISE_AND,         // &
+    NTT_BITWISE_OR,          // |
+    NTT_BITWISE_XOR,         // ^
+    NTT_BITWISE_SHIFT_LEFT,  // <<
+    NTT_BITWISE_SHIFT_RIGHT, // >>
+
+    NTT_PLUS_ASSIGNMENT,     // +=
+    NTT_MINUS_ASSIGNMENT,    // -=
+    NTT_MULTIPLY_ASSIGNMENT, // *=
+    NTT_DIVIDE_ASSIGNMENT,   // /=
+    NTT_MODULO_ASSIGNMENT,   // %=
+
+    NTT_BITWISE_AND_ASSIGNMENT,         // &=
+    NTT_BITWISE_OR_ASSIGNMENT,          // |=
+    NTT_BITWISE_XOR_ASSIGNMENT,         // ^=
+    NTT_BITWISE_SHIFT_LEFT_ASSIGNMENT,  // <<=
+    NTT_BITWISE_SHIFT_RIGHT_ASSIGNMENT, // >>=
+
+    NTT_LOGICAL_AND, // &&
+    NTT_LOGICAL_OR,  // ||
+    NTT_ARROW,       // ->
+    NTT_INCREMENT,   // ++
+    NTT_DECREMENT,   // --
+
+    NTT_EQUAL,      // ==
+    NTT_LESS,       // <
+    NTT_GREATER,    // >
+    NTT_ASSIGNMENT, // =
+    NTT_NOT,        // !
+    NTT_QUESTION,   // ?
+
+    NTT_NOT_EQUAL,     // !=
+    NTT_LESS_EQUAL,    // <=
+    NTT_GREATER_EQUAL, // >=
+    NTT_DEFINE,        // :=
+    NTT_ELLIPSIS,      // ...
+
+    NTT_POINTER,
+    NTT_TYPE_NAME,
+    NTT_TYPEDEF,
+
+    // Delimiters
+    NTT_OPEN_PARENTHESIS,     // (
+    NTT_OPEN_SQUARE_BRACKET,  // [
+    NTT_OPEN_CURLY_BRACE,     // {
+    NTT_CLOSE_PARENTHESIS,    // )
+    NTT_CLOSE_SQUARE_BRACKET, // ]
+    NTT_CLOSE_CURLY_BRACE,    // }
+
+    NTT_COMMA,     // ,
+    NTT_PERIOD,    // .
+    NTT_SEMICOLON, // ;
+    NTT_COLON,     // :
+
+    // Keywords
+    NTT_AUTO,
+    NTT_BREAK,
+    NTT_CASE,
+    NTT_CONST,
+    NTT_CONTINUE,
+    NTT_PRINT,
+    NTT_TRUE,
+    NTT_FALSE,
+
+    NTT_DEFAULT,
+    NTT_DO,
+    // NTT_DEFER,
+    NTT_ELSE,
+    NTT_FOR,
+    NTT_ENUM,
+    NTT_EXTERN,
+    NTT_INT,
+    NTT_CHAR,
+    NTT_LONG,
+    NTT_REGISTER,
+    NTT_SHORT,
+    NTT_SIGNED,
+    NTT_SIZEOF,
+    NTT_STATIC,
+    NTT_STRUCT,
+    NTT_NTTDEF,
+    NTT_UNION,
+    NTT_UNSIGNED,
+    NTT_VOLATILE,
+    NTT_WHILE,
+
+    NTT_FUNCTION,
+    NTT_GOTO,
+    NTT_IF,
+    NTT_IMPORT,
+
+    NTT_RETURN,
+
+    NTT_SWITCH,
+    NTT_VARIABLE, // [A-Za-z_]+ [A-Za-z0-9_]*
+                  // built-in data types
+    NTT_VOID,
+    NTT_BOOL,
+    NTT_INT8,   // char
+    NTT_UINT8,  // unsigned char
+    NTT_INT16,  // short
+    NTT_UINT16, // unsigned short
+    NTT_INT32,  // int
+    NTT_UINT32, // unsigned int
+    NTT_INT64,  // long long
+    NTT_UINT64, // unsigned long long
+    NTT_FLT32,  // float
+    NTT_FLT64,  // double (64 bits)
+
+    // Internal
+    NTT_COUNT
+};
+
+struct NemToken;
+
+extern struct NemToken        *nem_token_create( enum NemTokenType type,
+                                                 const char       *lexeme );
+extern void                    nem_token_destroy( struct NemToken **token );
+extern const enum NemTokenType nem_token_type( struct NemToken *token );
+extern const char *const       nem_token_lexeme( struct NemToken *token );
+
+#ifdef __cplusplus
+}
+#endif
+#endif // TOKEN_H
