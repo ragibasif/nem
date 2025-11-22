16279b1dcbfd9ea4d0f4009b941d76ef49aeedff
Ragib Asif <ragibasif@tuta.io>
2025-11-22 15:37:57 -0500

    feat: add report tracking for malloc wrappers

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 
 #include "common.h"
 #include "dbg.h"
+#define NEM_MEMORY_REPORT
 
 struct nem_memory_block {
     struct nem_memory_block *next;
@@ -20,9 +21,14 @@ struct nem_memory_block {
 
 static struct nem_memory_block *block_head = NULL;
 
-static void *nem_malloc( size_t size ) {
-    assert( size >= 0 );
-    if ( size < 0 ) { return NULL; }
+extern void *nem_malloc( const size_t size );
+extern void *nem_realloc( void *old_ptr, const size_t size );
+extern void *nem_calloc( const size_t count, const size_t size );
+extern void  nem_free( void *ptr );
+extern void  nem_free_all( void );
+
+void *nem_malloc( const size_t size ) {
+    if ( !( size > 0 ) ) { return NULL; }
     struct nem_memory_block *ptr = malloc( sizeof *ptr + size );
     assert( ptr != NULL );
     ptr->next  = block_head;
@@ -32,36 +38,79 @@ static void *nem_malloc( size_t size ) {
     return (void *)ptr + sizeof *ptr;
 }
 
-static void *nem_realloc( void *ptr, size_t size ) {
-    if ( ptr == NULL ) { return nem_malloc( size ); }
+void *nem_realloc( void *old_ptr, const size_t size ) {
+    if ( old_ptr == NULL ) { return nem_malloc( size ); }
 
-    struct nem_memory_block *block = (struct nem_memory_block *)ptr;
+    struct nem_memory_block *block = (struct nem_memory_block *)old_ptr;
     block--;
-    if ( block->size >= size ) { return ptr; }
+    if ( block->size >= size ) { return old_ptr; }
     void *new_ptr = nem_malloc( size );
-    memcpy( new_ptr, ptr, block->size );
+    memcpy( new_ptr, old_ptr, block->size );
     return new_ptr;
 }
 
-static void *nem_calloc( size_t count, size_t size ) {
+void *nem_calloc( const size_t count, const size_t size ) {
     void *ptr = nem_malloc( count * size );
     memset( ptr, 0, count * size );
     return ptr;
 }
 
-static void nem_free( void *ptr ) {
+void nem_free( void *ptr ) {
     assert( ptr != NULL );
     struct nem_memory_block *new_ptr = (struct nem_memory_block *)ptr;
     new_ptr--;
     memset( ptr, 0, new_ptr->size );
 }
 
-static void nem_free_all( void ) {
+static size_t nem_allocation_count( void ) {
+    size_t                   count = 0;
+    struct nem_memory_block *block = block_head;
+    while ( block ) {
+        count++;
+        block = block->next;
+    }
+    return count;
+}
+
+static size_t nem_total_allocated( void ) {
+    size_t                   total = 0;
+    struct nem_memory_block *block = block_head;
+    while ( block ) {
+        total += block->size;
+        block = block->next;
+    }
+    return total;
+}
+
+static size_t nem_total_memory_usage( void ) {
+    size_t                   total = 0;
+    struct nem_memory_block *block = block_head;
+    while ( block ) {
+        total += sizeof *block + block->size;
+        block = block->next;
+    }
+    return total;
+}
+
+void nem_free_all( void ) {
+#ifdef NEM_MEMORY_REPORT
+    size_t allocation_count       = nem_allocation_count();
+    size_t total_memory_allocated = nem_total_allocated();
+    size_t total_memory_usage     = nem_total_memory_usage();
+    fprintf( stdout,
+             "Nem Memory Report:\nAllocation Count: %zu\nMemory Allocated: "
+             "%zu\nMemory Usage: %zu\n",
+             allocation_count, total_memory_allocated, total_memory_usage );
+#endif
     while ( block_head ) {
         struct nem_memory_block *next_block = block_head->next;
         free( block_head );
         block_head = next_block;
     }
 }
-
-int main( int argc, char **argv ) { return EXIT_SUCCESS; }
+int main( int argc, char **argv ) {
+    int *a = nem_malloc( 42342 );
+    nem_free( a );
+    nem_free_all();
+    return EXIT_SUCCESS;
+}
