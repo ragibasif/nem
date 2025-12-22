c8cd22292288f4fd7c471a1f0d677733260728a2
Ragib Asif <ragibasif@tuta.io>
2025-12-22 14:29:12 -0500

    refactor: move tokens to separate module

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #endif
 
 #include "dbg.h"
+#include "token.hpp"
 
 #include <fstream>
 #include <iostream>
@@ -21,154 +22,6 @@
 
 namespace nem {
 
-enum class TokenType {
-    // Special tokens
-    Error,
-    Eof,
-    Comment,
-
-    // Literals
-    Identifier, // main
-    Number,     // 12345
-    Digit,      // [0-9]
-    Integer,    // 12345
-    Double,
-    Float,     // 123.45
-    Character, // 'a'
-    String,    // "abc"
-    Constant,
-
-    // Operators
-    Plus,     // +
-    Minus,    // -
-    Multiply, // *
-    Divide,   // /
-    Modulo,   // %
-    Invert,   // ~
-
-    BitwiseAnd,        // &
-    BitwiseOr,         // |
-    BitwiseXor,        // ^
-    BitwiseShiftLeft,  // <<
-    BitwiseShiftRight, // >>
-
-    PlusAssignment,     // +=
-    MinusAssignment,    // -=
-    MultiplyAssignment, // *=
-    DivideAssignment,   // /=
-    ModuloAssignment,   // %=
-    InvertAssignment,   // ~=
-
-    BitwiseAndAssignment,        // &=
-    BitwiseOrAssignment,         // |=
-    BitwiseXorAssignment,        // ^=
-    BitwiseShiftLeftAssignment,  // <<=
-    BitwiseShiftRightAssignment, // >>=
-
-    LogicalAnd, // &&
-    LogicalOr,  // ||
-    Arrow,      // ->
-    Increment,  // ++
-    Decrement,  // --
-
-    Equal,      // ==
-    Less,       // <
-    Greater,    // >
-    Assignment, // =
-    Not,        // !
-    Question,   // ?
-
-    NotEqual,     // !=
-    LessEqual,    // <=
-    GreaterEqual, // >=
-    Ellipsis,     // ...
-    Define,       // :=
-
-    Pointer,
-    TypeName,
-    Typedef,
-
-    // Delimiters
-    OpenParenthesis,    // (
-    OpenSquareBracket,  // [
-    OpenCurlyBrace,     // {
-    CloseParenthesis,   // )
-    CloseSquareBracket, // ]
-    CloseCurlyBrace,    // }
-
-    Comma,       // ,
-    Period,      // .
-    Semicolon,   // ;
-    Colon,       // :
-    SingleQuote, // '
-    DoubleQuote, // "
-
-    // Keywords
-    Asm,
-    Auto,
-    Break,
-    Case,
-    Const,
-    Continue,
-    Print,
-    True,
-    False,
-
-    Default,
-    Do,
-    Else,
-    For,
-    Enum,
-    Extern,
-    Int,
-    Char,
-    Long,
-    Register,
-    Short,
-    Signed,
-    Sizeof,
-    Static,
-    Struct,
-    Undef,
-    Union,
-    Unsigned,
-    Volatile,
-    While,
-
-    Function,
-    Goto,
-    If,
-    Import,
-
-    Return,
-
-    Switch,
-    Variable, // [A-Za-z_]+ [A-Za-z0-9_]*
-              // BUILT-in data types
-    Void,
-    Bool,
-    Int8,   // char
-    Uint8,  // unsigned char
-    Int16,  // short
-    Uint16, // unsigned short
-    Int32,  // int
-    Uint32, // unsigned int
-    Int64,  // long long
-    Uint64, // unsigned long long
-    Flt32,  // float
-    Flt64,  // double (64 bits)
-            // Internal
-    Id,
-    Count
-};
-
-struct Token {
-    enum TokenType   type;
-    std::string      literal;
-    std::string_view lexeme;
-    unsigned int     line;
-};
-
 class Lexer {
   private:
     std::vector< Token > tokens;
@@ -178,24 +31,6 @@ class Lexer {
     unsigned long long int line;
     unsigned long long int column;
     size_t                 position;
-
-    void read() {
-        constexpr auto read_size = std::size_t( 4096 );
-        auto           stream    = std::ifstream( path.data() );
-        stream.exceptions( std::ios_base::badbit );
-
-        if ( not stream ) {
-            throw std::ios_base::failure( "file does not exist" );
-        }
-
-        auto out = std::string();
-        auto buf = std::string( read_size, '\0' );
-        while ( stream.read( &buf[0], read_size ) ) {
-            out.append( buf, 0, static_cast< size_t >( stream.gcount() ) );
-        }
-        out.append( buf, 0, static_cast< size_t >( stream.gcount() ) );
-        source = out;
-    }
 
     static bool is_alpha( const char ch ) {
         return ( 'a' <= ch && ch <= 'z' ) || ( 'A' <= ch && ch <= 'Z' ) ||
@@ -223,8 +58,7 @@ class Lexer {
     }
 
   public:
-    Lexer( std::string path ) : path( path ) {
-        read();
+    Lexer( std::string source ) : source( source ) {
         tokens   = std::vector< Token >();
         line     = 1;
         column   = 1;
@@ -235,15 +69,28 @@ class Lexer {
 
 } // namespace nem
 
+std::string read( std::string path ) {
+    constexpr auto read_size = std::size_t( 4096 );
+    auto           stream    = std::ifstream( path.data() );
+    stream.exceptions( std::ios_base::badbit );
+
+    if ( not stream ) { throw std::ios_base::failure( "file does not exist" ); }
+
+    auto out = std::string();
+    auto buf = std::string( read_size, '\0' );
+    while ( stream.read( &buf[0], read_size ) ) {
+        out.append( buf, 0, static_cast< size_t >( stream.gcount() ) );
+    }
+    out.append( buf, 0, static_cast< size_t >( stream.gcount() ) );
+    return out;
+}
+
 int main( [[maybe_unused]] int argc, [[maybe_unused]] char **argv ) {
 
     auto wc_start  = std::chrono::high_resolution_clock::now();
     auto cpu_start = std::clock();
 
-    nem::Token token;
-    token.type    = nem::TokenType::Arrow;
-    token.literal = "->";
-    nem::Lexer lexer( "src/main.cpp" );
+    nem::Lexer lexer( read( "src/main.cpp" ) );
 
     auto   wc_end  = std::chrono::high_resolution_clock::now();
     auto   cpu_end = std::clock();
---
+++ src/token.hpp
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <string>
+
+enum class Types {
+    // Special tokens
+    Error,
+    Eof,
+    Comment,
+
+    // Literals
+    Identifier, // main
+    Number,     // 12345
+    Digit,      // [0-9]
+    Integer,    // 12345
+    Double,
+    Float,     // 123.45
+    Character, // 'a'
+    String,    // "abc"
+    Constant,
+
+    // Operators
+    Plus,     // +
+    Minus,    // -
+    Multiply, // *
+    Divide,   // /
+    Modulo,   // %
+    Invert,   // ~
+
+    BitwiseAnd,        // &
+    BitwiseOr,         // |
+    BitwiseXor,        // ^
+    BitwiseShiftLeft,  // <<
+    BitwiseShiftRight, // >>
+
+    PlusAssignment,     // +=
+    MinusAssignment,    // -=
+    MultiplyAssignment, // *=
+    DivideAssignment,   // /=
+    ModuloAssignment,   // %=
+    InvertAssignment,   // ~=
+
+    BitwiseAndAssignment,        // &=
+    BitwiseOrAssignment,         // |=
+    BitwiseXorAssignment,        // ^=
+    BitwiseShiftLeftAssignment,  // <<=
+    BitwiseShiftRightAssignment, // >>=
+
+    LogicalAnd, // &&
+    LogicalOr,  // ||
+    Arrow,      // ->
+    Increment,  // ++
+    Decrement,  // --
+
+    Equal,      // ==
+    Less,       // <
+    Greater,    // >
+    Assignment, // =
+    Not,        // !
+    Question,   // ?
+
+    NotEqual,     // !=
+    LessEqual,    // <=
+    GreaterEqual, // >=
+    Ellipsis,     // ...
+    Define,       // :=
+
+    Pointer,
+    TypeName,
+    Typedef,
+
+    // Delimiters
+    OpenParenthesis,    // (
+    OpenSquareBracket,  // [
+    OpenCurlyBrace,     // {
+    CloseParenthesis,   // )
+    CloseSquareBracket, // ]
+    CloseCurlyBrace,    // }
+
+    Comma,       // ,
+    Period,      // .
+    Semicolon,   // ;
+    Colon,       // :
+    SingleQuote, // '
+    DoubleQuote, // "
+
+    // Keywords
+    Asm,
+    Auto,
+    Break,
+    Case,
+    Const,
+    Continue,
+    Print,
+    True,
+    False,
+
+    Default,
+    Do,
+    Else,
+    For,
+    Enum,
+    Extern,
+    Int,
+    Char,
+    Long,
+    Register,
+    Short,
+    Signed,
+    Sizeof,
+    Static,
+    Struct,
+    Undef,
+    Union,
+    Unsigned,
+    Volatile,
+    While,
+
+    Function,
+    Goto,
+    If,
+    Import,
+
+    Return,
+
+    Switch,
+    Variable, // [A-Za-z_]+ [A-Za-z0-9_]*
+              // BUILT-in data types
+    Void,
+    Bool,
+    Int8,   // char
+    Uint8,  // unsigned char
+    Int16,  // short
+    Uint16, // unsigned short
+    Int32,  // int
+    Uint32, // unsigned int
+    Int64,  // long long
+    Uint64, // unsigned long long
+    Flt32,  // float
+    Flt64,  // double (64 bits)
+            // Internal
+    Id,
+    Count
+};
+
+class Token {
+  private:
+    enum Types   type;
+    std::string  literal;
+    std::string  lexeme;
+    unsigned int line;
+
+  public:
+    Token( enum Types type, std::string literal, std::string lexeme,
+           unsigned int line )
+        : type( type ), literal( literal ), lexeme( lexeme ), line( line ) {}
+};
